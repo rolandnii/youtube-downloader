e999e14e585cb7558664e085ca95141177b9d32a
niigyan <rolalu.me@gmail.com>
2023-02-28 20:55:50 +0000

    test my github configuration

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 def startDownload():
-    pass
+    try:
+        ytLink = link.get()
+        
+    except:
+        pass
 # Sytem Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
